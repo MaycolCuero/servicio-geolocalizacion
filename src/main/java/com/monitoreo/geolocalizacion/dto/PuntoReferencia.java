9eb33e93e28d95761e5480a5a802d9842171ff3a
MaycolCuero <maicolcr97@hotmail.com>
2025-07-26 23:49:42 -0500

    Se optimiza legibilidad de los DTO

--- src/main/java/com/monitoreo/geolocalizacion/dto/PuntoReferencia.java
+++ src/main/java/com/monitoreo/geolocalizacion/dto/PuntoReferencia.java
@@ -1,48 +1,32 @@
 package com.monitoreo.geolocalizacion.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 
 /**
  * Representa un punto geográfico con coordenadas de latitud y longitud.
  * Esta clase se utiliza para definir ubicaciones en el sistema de geolocalización.
  */
+@Setter
+@Getter
 public class PuntoReferencia implements Serializable {
+    /**
+     * -- GETTER --
+     *  Obtiene la longitud geográfica del punto.
+     * -- SETTER --
+     *  Establece la longitud geográfica del punto.
+     *
+     */
     private Long longitud;
+    /**
+     * -- GETTER --
+     *  Obtiene la latitud geográfica del punto.
+     * -- SETTER --
+     *  Establece la latitud geográfica del punto.
+     *
+     */
     private Long latitud;
 
-    /**
-     * Obtiene la longitud geográfica del punto.
-     *
-     * @return Longitud en formato decimal.
-     */
-    public Long getLongitud() {
-        return longitud;
-    }
-
-    /**
-     * Establece la longitud geográfica del punto.
-     *
-     * @param longitud Valor decimal que representa la longitud.
-     */
-    public void setLongitud(Long longitud) {
-        this.longitud = longitud;
-    }
-
-    /**
-     * Obtiene la latitud geográfica del punto.
-     *
-     * @return Latitud en formato decimal.
-     */
-    public Long getLatitud() {
-        return latitud;
-    }
-
-    /**
-     * Establece la latitud geográfica del punto.
-     *
-     * @param latitud Valor decimal que representa la latitud.
-     */
-    public void setLatitud(Long latitud) {
-        this.latitud = latitud;
-    }
 }
--- src/main/java/com/monitoreo/geolocalizacion/dto/ServicioGeolocalizacionInDTO.java
+++ src/main/java/com/monitoreo/geolocalizacion/dto/ServicioGeolocalizacionInDTO.java
@@ -1,86 +1,48 @@
 package com.monitoreo.geolocalizacion.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 
 /**
  * DTO utilizado para encapsular los datos de entrada relacionados con el servicio de geolocalización.
  * Contiene información sobre el vehículo, su ubicación actual, punto de partida y destino.
  */
+@Setter
+@Getter
 public class ServicioGeolocalizacionInDTO implements Serializable {
+    /**
+     * -- GETTER --
+     *  Obtiene el ID del vehículo.
+     * -- SETTER --
+     *  Establece el ID del vehículo.
+     *
+     */
     private Long idVehiculo;
+    /**
+     * -- GETTER --
+     *  Obtiene el punto de partida del vehículo.
+     * -- SETTER --
+     *  Establece el punto de partida del vehículo.
+     *
+     */
     private PuntoReferencia puntoPartida;
+    /**
+     * -- GETTER --
+     *  Obtiene el punto de llegada del vehículo.
+     * -- SETTER --
+     *  Establece el punto de llegada del vehículo.
+     *
+     */
     private PuntoReferencia puntoLlegada;
+    /**
+     * -- GETTER --
+     *  Obtiene la ubicación actual del vehículo.
+     * -- SETTER --
+     *  Establece la ubicación actual del vehículo.
+     *
+     */
     private PuntoReferencia ubicacionActual;
 
-    /**
-     * Obtiene el ID del vehículo.
-     *
-     * @return ID del vehículo.
-     */
-    public Long getIdVehiculo() {
-        return idVehiculo;
-    }
-
-    /**
-     * Establece el ID del vehículo.
-     *
-     * @param idVehiculo ID del vehículo.
-     */
-    public void setIdVehiculo(Long idVehiculo) {
-        this.idVehiculo = idVehiculo;
-    }
-
-    /**
-     * Obtiene el punto de partida del vehículo.
-     *
-     * @return Objeto {@link PuntoReferencia} correspondiente al punto de partida.
-     */
-    public PuntoReferencia getPuntoPartida() {
-        return puntoPartida;
-    }
-
-    /**
-     * Establece el punto de partida del vehículo.
-     *
-     * @param puntoPartida Objeto {@link PuntoReferencia} que representa el punto de partida.
-     */
-    public void setPuntoPartida(PuntoReferencia puntoPartida) {
-        this.puntoPartida = puntoPartida;
-    }
-
-    /**
-     * Obtiene el punto de llegada del vehículo.
-     *
-     * @return Objeto {@link PuntoReferencia} correspondiente al punto de llegada.
-     */
-    public PuntoReferencia getPuntoLlegada() {
-        return puntoLlegada;
-    }
-
-    /**
-     * Establece el punto de llegada del vehículo.
-     *
-     * @param puntoLlegada Objeto {@link PuntoReferencia} que representa el punto de llegada.
-     */
-    public void setPuntoLlegada(PuntoReferencia puntoLlegada) {
-        this.puntoLlegada = puntoLlegada;
-    }
-
-    /**
-     * Obtiene la ubicación actual del vehículo.
-     *
-     * @return Objeto {@link PuntoReferencia} que representa la ubicación actual.
-     */
-    public PuntoReferencia getUbicacionActual() {
-        return ubicacionActual;
-    }
-
-    /**
-     * Establece la ubicación actual del vehículo.
-     *
-     * @param ubicacionActual Objeto {@link PuntoReferencia} que representa la ubicación actual.
-     */
-    public void setUbicacionActual(PuntoReferencia ubicacionActual) {
-        this.ubicacionActual = ubicacionActual;
-    }
 }
